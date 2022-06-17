cb778f88414304337a4fd2684c691f56eede754a
ticofookfook <99700348+ticofookfook@users.noreply.github.com>
2022-06-17 16:09:23 -0300

    Add files via upload
    Simples e rapido com random ips

---
+++ nmapbypass.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\e[1;41;1;45m[+]COLOQUE O IP[+]\e[0m" 
+
+read ip
+
+echo -e "\e[1;41;1;45m[+]COLOQUE [-p-] SE QUISER VARRER TODAS AS PORTAS SE QUISER SO AS TOP 1000 DER UM ENTER![+]\e[0m"
+
+read a
+
+echo -e "\e[1;41;1;45m[+]SE QUISER A VERSÃO DOS SYSTEMA COLOQUE [C] [+]\e[0m"
+
+read c
+nmap -v -sS$c --open -Pn $a -D RND:20  $ip;
+
+
+
