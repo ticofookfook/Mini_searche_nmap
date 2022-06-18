53e752d93f68dbe62488b43272bb55079d6e63ae
ticofookfook <99700348+ticofookfook@users.noreply.github.com>
2022-06-18 09:03:28 -0300

    Add files via upload
    So  installar  o xte caso n tenha!

---
+++ estabilizar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source "/opt/exec/funcao.sh"
+
+hide_guake
+call_cmd "python -c 'import pty; pty.spawn(\"/bin/bash\")'"
+ctrl Z
+call_cmd "stty raw -echo"
+call_cmd "fg"
+call_cmd "export TERM=xterm"
